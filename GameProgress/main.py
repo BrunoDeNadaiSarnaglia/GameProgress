981706031fe9050604a0ddcde594d13ae1908d2b
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-06-19 12:15:11 -0300

    ClassYtransformer Done

--- GameProgress/main.py
+++ GameProgress/main.py
@@ -2,6 +2,7 @@ import numpy
 from pandas import DataFrame
 from sklearn.base import TransformerMixin
 from sklearn.feature_extraction.text import CountVectorizer
+from Pipeline.ClassYTransformer import ClassYTransformer
 from Pipeline.ImputerTransformer import ImputerTransformer
 from Pipeline.ColumnNameTransformer import ColumnNameTransformer
 from Pipeline.DateTimeEncoderTransformer import DateTimeEncoderTransformer
@@ -14,13 +15,15 @@ from Pipeline.RevenueTransformer import RevenueTransformer
 from Pipeline.UnitsTransformer import UnitsTransformer
 from sklearn.pipeline import Pipeline, FeatureUnion
 from Data.LoadTrainingData import TrainingData
-
+from sklearn import linear_model, decomposition, datasets
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
 
 def main():
-    data = TrainingData()
-    X = data.get()
-    D = DataFrame(X)
-    pipeline = Pipeline(
+    file = TrainingData()
+    data = file.get()
+    D = DataFrame(data)
+    pipelineX = Pipeline(
         [
             ('NullToNaN', NullTransformer()),
             ('featureType', FeatureUnion([
@@ -38,7 +41,9 @@ def main():
                 ('tbs', IntTransformer('tbs')),
                 ('tsad', IntTransformer('tsad'))
             ])),
-            ('ColumnNames1', ColumnNameTransformer(['revenue', 'units', 'ls_date', 'tsls', 'rating', 'ttp', 'total_sessions', 'completed', 'win_rate', 'tries', 'device', 'tbs', 'tsad'])),
+            ('ColumnNames1', ColumnNameTransformer(
+                ['revenue', 'units', 'ls_date', 'tsls', 'rating', 'ttp', 'total_sessions', 'completed', 'win_rate',
+                 'tries', 'device', 'tbs', 'tsad'])),
             ('featureImputer', FeatureUnion([
                 ('ls_date_imputer', ImputerTransformer('ls_date', 'most_frequent')),
                 ('tsls_imputer', ImputerTransformer('tsls', 'mean')),
@@ -46,32 +51,47 @@ def main():
                 ('completed_imputer', ImputerTransformer('completed', 'mean')),
                 ('win_rate_imputer', ImputerTransformer('win_rate', 'mean')),
                 ('tries_imputer', ImputerTransformer('tries', 'mean')),
-                ('repeater', RepeaterTransformer(['revenue', 'units', 'rating', 'ttp', 'total_sessions','device', 'tsad'])),
+                ('repeater',
+                 RepeaterTransformer(['revenue', 'units', 'rating', 'ttp', 'total_sessions', 'device', 'tsad'])),
             ])),
-            ('ColumnNames2', ColumnNameTransformer(['ls_date', 'tsls', 'tbs', 'completed', 'win_rate', 'tries', 'revenue', 'units', 'rating', 'ttp', 'total_sessions', 'device', 'tsad']))
+            ('ColumnNames2', ColumnNameTransformer(
+                ['ls_date', 'tsls', 'tbs', 'completed', 'win_rate', 'tries', 'revenue', 'units', 'rating', 'ttp',
+                 'total_sessions', 'device', 'tsad']))
         ]
     )
-    print pipeline.transform(D)
+    pipelineY = Pipeline([
+        ('NullToNaN', NullTransformer()),
+        ('ClassY', ClassYTransformer())
+    ])
+    pipelineFit = Pipeline([
+        ('logistic', linear_model.LogisticRegression())
+    ])
+    X = pipelineX.transform(D)
+    y = pipelineY.transform(D)
+    n_components = [20, 40, 64]
+    Cs = numpy.logspace(-4, 4, 3)
+
+    estimator = GridSearchCV(pipelineFit,
+                             dict(pca__n_components=n_components,
+                                  logistic__C=Cs))
+    estimator.fit(X, y)
+    print estimator.best_estimator_.score
+
 
 main()
 
-from sklearn import linear_model, decomposition, datasets
-from sklearn.pipeline import Pipeline
-from sklearn.grid_search import GridSearchCV
 
 logistic = linear_model.LogisticRegression()
 
 pca = decomposition.PCA()
 pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
 
-
-
 digits = datasets.load_digits()
 X_digits = digits.data
 print X_digits
 y_digits = digits.target
 
-###############################################################################
+# ##############################################################################
 # Plot the PCA spectrum
 pca.fit(X_digits)
 
@@ -81,8 +101,8 @@ pca.fit(X_digits)
 n_components = [20, 40, 64]
 Cs = numpy.logspace(-4, 4, 3)
 
-
 estimator = GridSearchCV(pipe,
                          dict(pca__n_components=n_components,
                               logistic__C=Cs))
 estimator.fit(X_digits, y_digits)
+
